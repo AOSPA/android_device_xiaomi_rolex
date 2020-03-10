73207b1e598bb1a1bfe9caef524c342e3fc17b52
Arian <arian.kulmer@web.de>
2020-03-10 20:41:46 +0100

    rolex: Drop power HAL from device tree
    * defined in source
    
    Change-Id: I47a00f40e7d43a9daa51add44cb456c7d4b82518

--- device.mk
+++ device.mk
@@ -275,10 +275,7 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(DEVICE_PATH)/configs/perf/perfconfigstore.xml:$(TARGET_COPY_OUT_VENDOR)/etc/perf/perfconfigstore.xml
 
-# Power
-PRODUCT_PACKAGES += \
-    android.hardware.power@1.2-service
-
+# Powerhint
 PRODUCT_COPY_FILES += \
     $(DEVICE_PATH)/configs/powerhint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.xml
 
